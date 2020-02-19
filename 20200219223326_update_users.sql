5743bbc48df2c1022b8431aa396d77a4daf4ca60
mojoboss <animusmojo@gmail.com>
2020-02-19 22:39:04 +0530

    added update function

---
+++ 20200219223326_update_users.sql
@@ -0,0 +1,19 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION users_db.update_user(userid INTEGER, firstName VARCHAR(64), lastName VARCHAR(64), emailId VARCHAR(256))
+    RETURNS VOID AS $$
+BEGIN
+    UPDATE users_db.users SET first_name = firstName, last_name = lastName, email = emailId
+    WHERE id = userid;
+END;
+$$
+    LANGUAGE plpgsql;
+-- +goose StatementEnd
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+
+DROP FUNCTION users_db.update_user(userid INTEGER, firstName VARCHAR(64), lastName VARCHAR(64), emailId VARCHAR(256));
